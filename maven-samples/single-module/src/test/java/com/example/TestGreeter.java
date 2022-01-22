2b7276d61eb9e0ae0d8246eaff40de6ec6d645bd
UllasRepo <97588439+UllasRepo@users.noreply.github.com>
2022-01-22 14:11:36 +0530

    Update TestGreeter.java
    Deleting test method - Extra

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,10 +41,4 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
  
-  @Test
-  public void greetShouldIncludeGreetingExtra() {
-    String someone = "World, Thank you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
